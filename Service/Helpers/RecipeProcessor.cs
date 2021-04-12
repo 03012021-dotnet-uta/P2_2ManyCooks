de60d6305f073a8711fc7083d6061a541e8e5854
Anis1988 <44283678+Anis1988@users.noreply.github.com>
2021-04-11 20:33:09 -0700

    added a recipeprocessor and recipe model

--- Service/Helpers/RecipeProcessor.cs
+++ Service/Helpers/RecipeProcessor.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Net.Http;
+using System.Net.Http.Headers;
 using System.Net.Http.Json;
 using System.Text;
 using System.Threading.Tasks;
@@ -10,30 +11,28 @@ using Repository.Models;
 namespace Service.Helpers
 {
     public class RecipeProcessor {
-        public static async Task<Recipe> LoadRecipe()
+        public static async Task<RecipeModel> LoadRecipe(string search)
         {
-            
-            var url = new HttpRequestMessage
+            var client = new HttpClient();
+            client.DefaultRequestHeaders.Accept.Clear();
+            client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+            var request = new HttpRequestMessage
             {
                 Method = HttpMethod.Get,
-                RequestUri = new Uri("https://nutritionix-api.p.rapidapi.com/v1_1/search/cheddar%20cheese?fields=item_name%2Citem_id%2Cbrand_name%2Cnf_calories%2Cnf_total_fat"),
+                RequestUri = new Uri($"https://nutritionix-api.p.rapidapi.com/v1_1/search/ {search}?fields=item_name%2Citem_id%2Cbrand_name%2Cnf_calories%2Cnf_total_fat"),
                 Headers =
                 {
                     { "x-rapidapi-key", "e157b8d687msh431e30623e70dd3p174a1cjsn7ea0d090c0f9" },
                     { "x-rapidapi-host", "nutritionix-api.p.rapidapi.com" },
                 },
             };
-            using (HttpResponseMessage response = await ApiHelper.ApiClient.SendAsync(url))
+            using (var response = await client.SendAsync(request))
             {
-                if (response.IsSuccessStatusCode)
-                {
-                    Recipe recipe = await response.Content.ReadFromJsonAsync<Recipe>();
-
-                    return recipe;
-                }
-
-                throw new Exception(response.ReasonPhrase);
+                response.EnsureSuccessStatusCode();
+                var body = await response.Content.ReadFromJsonAsync<RecipeModel>();
+                return body;
             }
+
         }
     }
 }
